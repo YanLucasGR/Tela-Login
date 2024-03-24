e6d503109092a2bac54a0a05fbda4936ea4c1554
YanLucasGR <gondimrabeloyanlucas@gmail.com>
2024-03-24 11:55:00 -0300

    Create Tela-Login

---
+++ Login/login.js
@@ -0,0 +1,9 @@
+function Enviar(){
+    var nome = document.getElementById('nome')
+    var senha = document.getElementById('senha')
+
+    let valor = nome.value
+
+    alert(`Logado
+Bem vindo de volta ${valor}!`)
+}
